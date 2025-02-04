15771b9a37767621b467d77c272cdb2697122d63
alasarerhan <alasar.erhan@gmail.com>
2025-02-04 16:49:57 +0300

    ARIMA model is studied from co2 dataset

--- statistical_time_series_methods.py
+++ statistical_time_series_methods.py
@@ -45,6 +45,113 @@ ARIMA is suitable for data with univariate trend but no seasonality.
 p: number of real value lags (autoregressive degree) If p = 2, yt-1 and yt-2 are in the model.
 d: number of difference operations (difference degree, I)
 q: number of error lags (moving average degree)
-
 """
 
+#?################################
+#? ARIMA MODEL
+#?################################
+import matplotlib.pyplot as plt
+import itertools
+import warnings
+import numpy as np
+import pandas as pd 
+import statsmodels.api as sm
+from sklearn.metrics import mean_absolute_error
+from statsmodels.tsa.arima.model import ARIMA
+#from statsmodels.tsa.holtwinters import ExponentialSmoothing
+#from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.tsa.api as smt 
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+warnings.filterwarnings("ignore")
+
+
+#############################
+# DATASET
+#############################
+
+# Atmospheric CO2 from Continuous Air Samples at Mauna Loa Observatory, Hawaii, U.S.A.
+# Period of record: March 1958 - December 2001
+
+data = sm.datasets.co2.load_pandas()
+y = data.data 
+y = y['co2'].resample('MS').mean()
+y = y.fillna(y.bfill())
+train = y[:'1997-12-01']
+test = y['1998-01-01':]
+
+#################################################################################
+# ARIMA(p,d,q): (Autoregressive Integrated Moving Average)
+#################################################################################
+
+#!For AR, MA and ARMA models, the data set should be stationary.
+#! Not suitable for data with seasonality and trends
+#! The ARIMA model can also be used in trended series by taking the difference.
+arima_model = ARIMA(train, order = (1,1,1)).fit()
+
+
+#? Statistical Output of the model
+print(arima_model.summary())
+
+#? Predictions
+y_pred = arima_model.forecast(48)[0]
+print(y_pred)
+
+#? Visualization
+
+def plot_co2(train, test, y_pred, title):
+    mae = mean_absolute_error(test, y_pred)
+    train["1985":].plot(legend=True, label="TRAIN", title=f"{title} - MAE: {mae:.2f}")
+    test.plot(legend=True, label="TEST", figsize=(6,4))
+    y_pred.plot(legend=True, label="PREDICTION")
+    plt.show()
+
+
+y_pred = pd.Series(y_pred, index = test.index)
+
+plot_co2(train, test, y_pred, "ARIMA")
+
+#! The ARIMA model is a sibling of the DES (Double Exponential Smoothing) model.
+#! DES can model the trend, ARIMA can also model the trend.
+
+
+#?#############################################################
+#? Hyperparameter Optimization (Determining model degrees)
+#?#############################################################
+
+#!############################################################
+#! Determining Model Degree According to AIC & BIC Statistics
+#!############################################################
+
+#? AIC (Akaike Information Criterion) -> the lower the better
+
+p = d = q = range(0,4)
+pdq = list(itertools.product(p,d,q))
+
+def arima_optimizer(train, orders):
+    best_aic, best_params = float("inf"), None
+    for order in orders:
+        try:
+            arima_model_result = ARIMA(train, order = order).fit()
+            aic = arima_model_result.aic
+            if aic < best_aic:
+                best_aic, best_params = aic, order
+            print("ARIMA%s AIC=%.2f" % (order, aic))
+        except:
+            continue
+    print("Best ARIMA%s AIC=%.2f" % (best_params, best_aic))
+    return best_params
+
+best_params_aic = arima_optimizer(train, pdq)
+
+#?#####################
+#? Final Model
+#?#######################
+
+arima_model = ARIMA(train, best_params_aic).fit()
+y_pred = arima_model.forecast(steps=48)
+y_pred = pd.Series(y_pred, index = test.index)
+
+
+plot_co2(train, test, y_pred, "ARIMA")
---
+++ time_series_funcs.py
@@ -0,0 +1,217 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.metrics import mean_absolute_error
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+
+#! Durağanlık testi (Dickey-Fuller testi)
+def is_stationary(y):
+
+    # H0: Zaman serisi durağan değildir.
+    # H1: Zaman serisi durağandır.
+
+    p_value = sm.tsa.stattools.adfuller(y)[1]
+    if p_value < 0.05:
+        print(f"Result: Stationary (H0: non stationary, p value: {round:p_value,3})")
+    else:
+        print(f"Result: Non Stationary (H0: stationary, p value: {round:p_value,3})")
+
+
+#! Zaman serisi bileşenleri ve durağanlık testi
+def ts_decompose(y, model='additive',stationary=False):
+    result = seasonal_decompose(y, model=model)
+
+    fig,axes = plt.subplots(4,1, sharex=True, sharey=False)
+    fig.set_figheight(10)
+    fig.set_figwidth(15)
+
+    axes[0].set_title('Decomposition for ' + model + " model")
+    axes[0].plot(y, 'k', label='Original' + model)
+    axes[0].legend(loc='upper left')
+
+    axes[1].plot(result.trend, label='Trend')
+    axes[1].legend(loc='upper left')
+
+    axes[2].plot(result.seasonal,'g', label='Seasonality & Mean : ' + str(round(result.seasonal.mean(), 4)))
+    axes[2].legend(loc='upper left')
+
+    axes[3].plot(result.resid, 'r', label='Residuals & Mean : ' + str(round(result.resid.mean(), 4)))
+    axes[3].legend(loc='upper left')
+
+    plt.tight_layout()
+    plt.show(block=True)
+
+    if stationary:s
+        is_stationary(y)
+
+
+def plot_co2(train, test, y_pred, title):
+    mae = mean_absolute_error(test, y_pred)
+    train["1985":].plot(legend=True, label="TRAIN", title=f"{title} - MAE: {mae:.2f}")
+    test.plot(legend=True, label="TEST", figsize=(6,4))
+    y_pred.plot(legend=True, label="PREDICTION")
+    plt.show()
+
+
+#! Single Exponential Smoothing Optimizer Function
+def ses_optimizer(train, alphas, step=48):
+    best_alpha, best_mae = None, float("inf")
+    for alpha in alphas:
+        mpdel = SimpleExpSmoothing(train).fit(smoothing_level=alpha)
+        y_pred = model.forecast(step)
+        mae = mean_absolute_error(test, y_pred)
+        if mae < best_mae:
+            best_alpha, best_mae = alpha, mae
+        print("alpha:", round(alpha,2), "mae:", round(mae,2))
+    print("Best alpha:", round(best_alpha,2), "Best MAE:", round(best_mae,2))
+    return best_alpha, best_mae
+
+
+#! Double Exponential Smoothing Optimizer Function
+def des_optimizer(train, alphas, betas, step=48):
+    best_alpha, best_beta, best_mae = None, None, float("inf")
+    for alpha in alphas:
+        for beta in betas:
+            model = ExponentialSmoothing(train, trend='add').fit(smoothing_level=alpha, smoothing_trend=beta)
+            y_pred = model.forecast(step)
+            mae = mean_absolute_error(test, y_pred)
+            if mae < best_mae:
+                best_alpha, best_beta, best_mae = alpha, beta, mae
+            print("alpha:", round(alpha,2), "beta:", round(beta,2), "mae:", round(mae,4))
+    print("Best alpha:", round(best_alpha,2), "Best beta:", round(best_beta,2), "Best MAE:", round(best_mae,4))
+    return best_alpha, best_beta, best_mae
+
+
+
+
+#! Triple Exponential Smoothing Optimizer Function
+def tes_optimizer(train, alphas, betas, gammas, step=48):
+    best_alpha, best_beta, best_gamma, best_mae = None, None, None, float("inf")
+    for alpha in alphas:
+        for beta in betas:
+            for gamma in gammas:
+                model = ExponentialSmoothing(train, trend='add', seasonal='add', seasonal_periods=12).fit(smoothing_level=alpha, smoothing_trend=beta, smoothing_seasonal=gamma)
+                y_pred = model.forecast(step)
+                mae = mean_absolute_error(test, y_pred)
+                if mae < best_mae:
+                    best_alpha, best_beta, best_gamma, best_mae = alpha, beta, gamma, mae
+                print("alpha:", round(alpha,2), "beta:", round(beta,2), "gamma:", round(gamma,2), "mae:", round(mae,4))
+    print("Best alpha:", round(best_alpha,2), "Best beta:", round(best_beta,2), "Best gamma:", round(best_gamma,2), "Best MAE:", round(best_mae,4))
+    return best_alpha, best_beta, best_gamma, best_mae
+
+
+def check_df(dataframe, head=5, tail=5, quan=False):
+    print('########## Shape ##########')
+    print(dataframe.shape)
+    print('########## Types ##########')
+    print(dataframe.dtypes)
+    print("##################### Duplicated Values #####################")
+    print(dataframe.duplicated().sum())
+    print('########## Head ##########')
+    print(dataframe.head(head))
+    print('########## Tail ##########')
+    print(dataframe.tail(tail))
+    print('########## NA ##########')
+    print(dataframe.isnull().sum())
+    if quan:
+        print('########## Quantiles ##########')
+        print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+
+#! Creating time features
+
+def create_date_features(dataframe):
+    dataframe['MONTH'] = dataframe.date.dt.MONTH
+    dataframe['DAY_OF_MONTH'] = dataframe.date.dt.DAY_OF_MONTH
+    dataframe['DAY_OF_YEAR'] = dataframe.date.dt.DAY_OF_YEAR
+    dataframe['WEEK_OF_YEAR'] = dataframe.date.dt.WEEK_OF_YEAR
+    dataframe['DAY_OF_WEEK'] = dataframe.date.dt.DAY_OF_WEEK
+    dataframe['YEAR'] = dataframe.date.dt.YEAR
+    dataframe['IS_WEEKEND'] = dataframe.date.dt.weekday // 4
+    dataframe['IS_MONTH_START'] = dataframe.date.dt.IS_MONTH_START.astype(int)
+    dataframe['IS_MONTH_END'] = dataframe.date.dt.IS_MONTH_END.astype(int)
+
+    return dataframe
+
+
+#! lag_features
+def lag_features(dataframe, lags):
+    for lag in lags:
+        dataframe['sales_lag_' +str(lag)] = dataframe.groupby(['store','item'])['sales'].transform(
+            lambda x:x.shift(lag)) + random_noise(dataframe)
+    return dataframe
+
+
+#! rolling mean windows
+def roll_mean_feature(dataframe,windows):
+    for window in windows:
+        dataframe['sales_roll_mean_' + str(window)] = dataframe.groupby(['store','item'])['sales'].\
+            transform(lambda x:x.shift(1).rolling(window=window, min_periods=10, win_type="triang")).mean().\
+                + random_noise(dataframe)
+    return dataframe
+
+ 
+#! Exponentially Weighted Features
+def ewm_features(dataframe, alphas, lags):
+    for alpha in alphas:
+        for lag in lags:
+            dataframe['sales_ewm_alpha_' + str(alpha).replace(".","")+"_lag_" + str(lag)] = \
+                dataframe.groupby(["store",'item'])['sales'].transform(lambda x:x.shift(lag).ewm(alpha=alpha).mean())
+    return dataframe
+
+#! SMAPE calculation (Custom Cost Function)
+def smape(preds, target):
+    n = len(preds)
+    masked_arr = ~((preds==0) & (target==0))
+    preds, target = preds[masked_arr], target[masked_arr]
+    num = np.abs(preds-target)
+    denom = np.abs(preds) + np.abs(target)
+    smape_val = (200*np.sum(num/denom)) / n
+    return smape_val
+
+
+#! SMAPE ile model değerlendirme
+def lgbm_smape(preds,train_data):
+    labels = train_data.get_label()
+    smape_val = smape(np.expm1(preds), np.expm1(labels))
+    return 'SMAPE', smape_val, False
+
+
+#! feature importances
+def plot_lgb_importances(model, plot=False,num=10):
+    gain = model.feature_importance('gain')
+    feat_imp = pd.DataFrame({'feature':model.feature_name(),
+                            'split':model.feature_importance('split'),
+                            'gain': 100 * gain / gain.sum()}).sort_values('gain', ascending=False)
+    
+    if plot:
+        plt.figure(figsize=(10,10))
+        sns.set(font_scale = 1)
+        sns.barplot(x="gain", y ="feature",data=feat_imp[0:25])
+        plt.title('feature')
+        plt.tight_layout()
+        plt.show()
+    else:
+        print(feat_imp.head(num))
+
+
+#! ARIMA Optimizer Function
+def arima_optimizer(train, orders):
+    best_aic, best_params = float("inf"), None
+    for order in prders:
+        try:
+            arima_model_result = ARIMA(train, order = order).fit()
+            aic = arima_model_result.aic
+            if aic < best_aic:
+                best_aic, best_params = aic, order
+            print("ARIMA%s AIC=%.2f" % (order, aic))
+        except:
+            continue
+    print("Best ARIMA%s AIC=%.2f" % (best_params, best_aic))
+    return best_params
